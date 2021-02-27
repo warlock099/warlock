44409e0c90f5648bbc9371aee8d5db5580919444
aaronzomback <aaronzomback@gmail.com>
2021-02-27 16:51:46 +0100

    removing and cleaning migrations

--- db/migrate/20200917092620_add_product_variant_to_order_item.rb
+++
@@ -1,5 +0,0 @@
-class AddProductVariantToOrderItem < ActiveRecord::Migration[6.0]
-  def change
-    add_column :order_items, :product_variant_id, :integer
-  end
-end
--- db/migrate/20200918224913_remove_product_id_from_order_item.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductIdFromOrderItem < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :order_items, :product_id, :integer
-  end
-end
