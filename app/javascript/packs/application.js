f26e35d77ae3b279b48701ff65a0bde9559b28fe
aaronzomback <aaronzomback@gmail.com>
2021-05-06 20:35:15 +0200

    hamburger menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require("packs/main")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
